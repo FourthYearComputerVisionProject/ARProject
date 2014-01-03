6a0166b544aa57e079139957523fe06889da01e3
KMadore <kemadore@gmail.com>
2014-01-02 23:57:51 -0500

    Created HandDetection.cpp and HandDetection.h.  Re-organizing my code into the classes.

---
+++ OpenCVwithOpenGL/HandDetection.cpp
@@ -0,0 +1,23 @@
+/**
+	HandDection - detect the hand and provide recognition for gestures
+	
+	Author: Karen Madore
+	Date:	Jan 2, 2013
+	Version: 0.03
+
+*/
+
+#include "HandDetection.h"
+#include <opencv\highgui.h>
+#include <opencv\cv.h>
+#include <sstream>
+#include <iostream>
+#include <string>
+
+HandDetection::HandDetection()
+{
+}
+
+HandDetection::~HandDetection()
+{
+}
---
+++ OpenCVwithOpenGL/HandDetection.h
@@ -0,0 +1,33 @@
+/**
+	HandDection - detect the hand and provide recognition for gestures
+	
+	Author: Karen Madore
+	Date:	Jan 2, 2013
+	Version: 0.03
+
+*/
+
+#ifndef HAND_DETECTION_H
+#define HAND_DETECTION_H
+
+#include <opencv\highgui.h>
+#include <opencv\cv.h>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace cv;
+class HandDetection
+
+{
+public:
+	HandDetection();
+	~HandDetection();
+
+private:
+
+}; //HandDetection
+
+#endif
