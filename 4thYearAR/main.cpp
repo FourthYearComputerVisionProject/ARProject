7c755e80eb05b57843c99fb5f3dfe5017bbb5e5a
KMadore <kemadore@gmail.com>
2014-01-15 19:59:12 -0500

    Commented out my manipulator in main.

--- 4thYearAR/main.cpp
+++ 4thYearAR/main.cpp
@@ -83,8 +83,8 @@ int main(int argc, char **argv) {
 	render = new StereoRender(capture);//0,0 for one cam//0,2 for rift
 
 	//add manipulators
-	SinglePointManipulator* spManipulator = new SinglePointManipulator();
-	render->addManipulator(spManipulator);
+	//SinglePointManipulator* spManipulator = new SinglePointManipulator();
+	//render->addManipulator(spManipulator);
 	
 	//glewInit();
 
